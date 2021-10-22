8704e9136332b1a6e12b1c327951420c4027bf9f
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-22 14:14:26 +0530

    Added: Flip Coin file is added

--- src/com/blp/basiccoreprograms/FlipCoin.java
+++ src/com/blp/basiccoreprograms/FlipCoin.java
@@ -10,12 +10,15 @@ public class FlipCoin {
 
     public static void main(String[] args) {
         FlipCoin coin = new FlipCoin();
+        //Create object to call method
         coin.flip();
+        //Call method to get count of head and tail
         System.out.println("Percentage of Heads and Tails is = " + coin.percentage() + " " + "&" + " " + (100 - coin.percentage()));
     }
 
     private void flip() {
         int numOfFlip;
+        //Use for loop to get count
         for (numOfFlip = 1; numOfFlip <= 50; numOfFlip++) {
             int randomNum = (int) (Math.floor(Math.random() * 10) % 2);
             if (randomNum == head) {
@@ -24,7 +27,7 @@ public class FlipCoin {
                 count2++;
         }
     }
-
+    // Calculate percentage of head and tail
     private double percentage() {
         double perHead = ((double) count1 / 50) * 100;
         return (perHead);
